77ea3cad51ddad1fd57dd2fdedf796b000b823b1
kim <smkim000721@naver.com>
2025-04-21 09:34:26 +0900

    Slf4j를 이용하여 로그출력

--- jpaboard/src/main/java/com/example/jpaboard/controller/HomeController.java
+++ jpaboard/src/main/java/com/example/jpaboard/controller/HomeController.java
@@ -4,11 +4,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Controller
 public class HomeController {
 	@GetMapping("/")
 	public String home(Model model) {
 		model.addAttribute("loginName", "aaa");
+		// log 프레임워크 사용
+		log.debug("loginName: "+model.getAttribute("loginName"));
 		return "home";
 	}
 }
