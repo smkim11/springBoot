2911e8de3744d96ada09bb449c4697a08a121532
kim <smkim000721@naver.com>
2025-04-22 12:03:45 +0900

    세션값을 이용한 로그인 로그아웃 연습

---
+++ jpaboard/src/main/java/com/example/jpaboard/controller/SessionTestController.java
@@ -0,0 +1,24 @@
+package com.example.jpaboard.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import jakarta.servlet.http.HttpSession;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+public class SessionTestController {
+	
+	@GetMapping("/newSession")
+	public String newSession(HttpSession session) { // request.getHttpSession()
+		session.setAttribute("sessionData", "GDJ91");
+		return "sessionTest";
+	}
+	
+	@GetMapping("/dropSession")
+	public String dropSession(HttpSession session) {
+		session.invalidate();
+		return "sessionTest";
+	}
+}
