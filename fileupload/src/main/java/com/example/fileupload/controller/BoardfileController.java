342854b6be5d89bab677f17ec0f56b4a5bca5c9e
kim <smkim000721@naver.com>
2025-04-24 14:42:11 +0900

    상세페이지에서 파일추가 할 수 있도록 수정

--- fileupload/src/main/java/com/example/fileupload/controller/BoardfileController.java
+++ fileupload/src/main/java/com/example/fileupload/controller/BoardfileController.java
@@ -1,12 +1,19 @@
 package com.example.fileupload.controller;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.example.fileupload.dto.BoardForm;
 import com.example.fileupload.entity.Boardfile;
 import com.example.fileupload.repository.BoardFileRepository;
 import com.example.fileupload.repository.BoardRepository;
@@ -31,6 +38,45 @@ public class BoardfileController {
 		
 		// 테이블 삭제
 		boardFileRepository.deleteById(fno);
+		return "redirect:/boardOne?bno="+bno;
+	}
+	
+	@GetMapping("/addFile")
+	public String addFile(Model model, @RequestParam int bno) {
+		
+		model.addAttribute("bno", bno);
+		return "addFile";
+	}
+	
+	@PostMapping("/addFile")
+	public String addFile(BoardForm boardForm, @RequestParam int bno) {
+		List<MultipartFile> list = boardForm.getFileList();
+		if(!list.get(0).isEmpty()) {
+			for(MultipartFile file : list) {
+				// 파일의 확장자 추출
+				String ext = file.getOriginalFilename().substring(file.getOriginalFilename().lastIndexOf(".")+1);
+				// 저장되는 파일이름 UUID로 다르게 설정
+				String fileName = UUID.randomUUID().toString().replace("-", "");
+				
+				// 실제 폴더 저장 위치
+				File saveFolder = new File("C:/project/upload/"+fileName+"."+ext);
+				
+				try {
+					file.transferTo(saveFolder);
+				} catch (IllegalStateException | IOException e) {
+					e.printStackTrace();
+				}
+				
+				Boardfile boardfile = new Boardfile();
+				boardfile.setForiginname(file.getOriginalFilename());
+				boardfile.setBno(bno);
+				boardfile.setFsize(file.getSize());
+				boardfile.setFext(ext);
+				boardfile.setFtype(file.getContentType());
+				boardfile.setFname(fileName);
+				boardFileRepository.save(boardfile);
+			}
+		}
 		return "redirect:/boardOne?bno="+bno;
 	}
 }
