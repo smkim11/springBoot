6eb50c43a6a39fee224c76a5b90d6fa0a8745f03
kim <smkim000721@naver.com>
2025-06-28 14:31:55 +0900

    DTO에서 Entity로 변환

--- api/src/main/java/com/sakila/api/dto/CityDto.java
+++ api/src/main/java/com/sakila/api/dto/CityDto.java
@@ -2,6 +2,8 @@ package com.sakila.api.dto;
 
 import java.sql.Timestamp;
 
+import com.sakila.api.entity.CityEntity;
+
 import lombok.Data;
 @Data
 public class CityDto {
@@ -10,9 +12,12 @@ public class CityDto {
 	private Timestamp lastUpdate;
 	private int countryId;
 	
-	/*
-	public CityEntity = new CityEntity() {
-		return "";
+	
+	public CityEntity toEntity(){
+		CityEntity entity = new CityEntity();
+		entity.setCity(this.city);
+		
+		return entity;
 	}
-	*/
+	
 }
